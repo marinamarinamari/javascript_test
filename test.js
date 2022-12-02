a3b0fbd27bb5a7faf272e66e11d9d9a2e4271c68
宮田真里奈 <marina@miyatamarinanoMacBook-Pro.local>
2022-12-02 14:50:59 +0900

    課題提出４回目

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-for (let i = 1; i <= 50; i++){
+let i ;
+for (i = 1; i <= 50; i++) {
   if(i % 10 === 0) {
     console.log("今" + i +"回ループしました。");
 } else if(i % 4 === 0) {
   console.log("４で割れる数です。" + i +"");
-} else if (i == 50) {
-  alert("５０回カウントが終わりました。");
+} else if(i === 50) {
+  console.log(i);
 }
 }
+alert("50回カウントが終わりました。");
